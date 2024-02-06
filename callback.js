caf4ae1c998ec3663f878d596edcabf784b117b9
martinpasco <martinpasco@hotmail.com>
2024-02-05 23:43:09 -0500

    task 4
    create function in task-04

---
+++ callback.js
@@ -0,0 +1,15 @@
+function callbackFunc() {
+    console.log("¡Esta es una función de callback!");
+  }
+
+
+/*
+
+function calculate(a, b, callback) {
+    callback(a + b);
+  }
+  
+  function displayResult(result) {
+    console.log('The result is: ' + result);
+  }
+  */
---
+++ scripts/w04-task.js
@@ -0,0 +1,46 @@
+/* LESSON 3 - Programming Tasks */
+let photoElement = document.querySelector(`#photo`);
+/* Profile Object  */  /* Populate Profile Object with placesLive objects */
+let myProfile = {
+    name: "Martin Pasco",
+    photo: {
+        src:"images/Martin.png",
+        alt: "My Profile Picture"
+    },
+    favoriteFoods: ["Chocolate", "Tacos", "Seco de Pollo"],
+    Hobies: ["ciclying", "Swiming", "soccer"],
+    placeLived: ["Lima- Perú", "Miami - USA"]
+};
+
+/* DOM Manipulation - Output */
+/* Name */
+document.querySelector("#name").innerHTML = `My name is <em>${myProfile.name}</em`;
+/* Photo with attributes */
+photoElement.src = myProfile.photo.src;
+photoElement.alt = myProfile.photo.alt;
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(item => {
+    let li = document.createElement("li");
+    li.textContent= item;
+    document.querySelector("#favorite-foods").appendChild(li);
+
+})
+
+/* Hobbies List */
+myProfile.Hobies.forEach(item => {
+    let li = document.createElement("li");
+    li.textContent= item;
+    document.querySelector("#hobbies").appendChild(li);
+
+})
+
+/* Places Lived DataList */
+myProfile.placeLived.forEach(item => {
+    let li = document.createElement("li");
+    li.textContent= item;
+    document.querySelector("#places-lived").appendChild(li);
+
+})
+
+
+
